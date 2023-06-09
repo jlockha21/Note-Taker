a42301a4797a0dfe3faec59e40c7c8bc04a36519
Jordan Lockhart <jordanlockhart556@gmail.com>
2023-06-08 21:58:21 -0700

    modified files for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Choose a port number of your choice
+const PORT = process.env.PORT || 3000; // Choose a port number of your choice
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
@@ -87,10 +87,10 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
